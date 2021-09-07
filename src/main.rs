79d63b72e1d749a9b2ccb60f873521c1b8e59168
Kazuaki Nomura <nkz0914ssk@gmail.com>
2021-09-07 22:04:34 +0900

    :package: (dep) add clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use clap::{load_yaml, App};
+
 fn main() {
-    println!("Hello, world!");
+    let yaml = load_yaml!("cli.yaml");
+    let m = App::from(yaml).get_matches();
+    if m.is_present("add") {
+        println!("'todo'");
+    }
+
+    if let Some(m) = m.subcommand_matches("add") {
+        println!("Adding todo: {}", m.value_of("content").unwrap());
+    }
 }
