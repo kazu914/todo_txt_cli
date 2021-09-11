14f0aa5dadcd2dbbf95e81d9148770533dd45bed
Kazuaki Nomura <nkz0914ssk@gmail.com>
2021-09-11 16:49:04 +0900

    :sparkles:  make some flags as option

--- src/model.rs
+++ src/model.rs
@@ -1,35 +1,43 @@
 pub struct Todo {
     pub content: String,
-    pub priority: String,
-    pub projects: Vec<String>,
-    pub contexts: Vec<String>,
+    pub priority: Option<String>,
+    pub projects: Option<Vec<String>>,
+    pub contexts: Option<Vec<String>>,
 }
 
 impl Todo {
     pub fn new(
         content: impl Into<String>,
-        priority: impl Into<String>,
-        projects: Vec<impl Into<String>>,
-        contexts: Vec<impl Into<String>>,
+        priority: Option<impl Into<String>>,
+        projects: Option<Vec<impl Into<String>>>,
+        contexts: Option<Vec<impl Into<String>>>,
     ) -> Todo {
         Todo {
             content: content.into(),
-            priority: priority.into(),
-            projects: projects.into_iter().map(|project| project.into()).collect(),
-            contexts: contexts.into_iter().map(|context| context.into()).collect(),
+            priority: priority.map(Into::into),
+            projects: projects
+                .map(|projects| projects.into_iter().map(|project| project.into()).collect()),
+            contexts: contexts
+                .map(|contexts| contexts.into_iter().map(|context| context.into()).collect()),
         }
     }
 
     pub fn to_formatted_string(&self) -> String {
-        let mut res: String = "(".to_string() + &self.priority.clone() + ")";
+        let mut res: String = "".to_string();
+        if let Some(priority) = &self.priority {
+            res += &("(".to_string() + &priority.to_string() + ")");
+        }
         res += &(" ".to_string() + &self.content.clone());
-        for project in &self.projects {
-            res += &(" +".to_string() + &project.to_string())
+        if let Some(projects) = &self.projects {
+            for project in projects {
+                res += &(" +".to_string() + project)
+            }
         }
-        for context in &self.contexts {
-            res += &(" @".to_string() + &context.to_string())
+        if let Some(contexts) = &self.contexts {
+            for context in contexts {
+                res += &(" @".to_string() + &context)
+            }
         }
-
-        return res;
+        res
     }
 }
--- src/service.rs
+++ src/service.rs
@@ -18,9 +18,9 @@ impl TodoService {
     }
     pub fn add_todo(&self, matches: &ArgMatches) {
         let content: &str = matches.value_of(CONTENT).unwrap();
-        let priority: &str = matches.value_of(PRIORITY).unwrap();
-        let projects: Vec<&str> = matches.values_of(PROJECTS).unwrap().collect();
-        let contexts: Vec<&str> = matches.values_of(CONTEXTS).unwrap().collect();
+        let priority: Option<&str> = matches.value_of(PRIORITY);
+        let projects: Option<Vec<String>> = matches.values_of_t(PROJECTS).ok();
+        let contexts: Option<Vec<String>> = matches.values_of_t(CONTEXTS).ok();
         let todo = Todo::new(content, priority, projects, contexts);
 
         &self.file.append(&todo.to_formatted_string());
