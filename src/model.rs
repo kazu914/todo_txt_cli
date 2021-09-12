8319b32f62869988252573facc50ec1ee292db15
Kazuaki Nomura <nkz0914ssk@gmail.com>
2021-09-12 21:11:55 +0900

    :sparkles: (todo model) add from_string

--- src/model.rs
+++ src/model.rs
@@ -1,3 +1,5 @@
+use super::helper::{is_context, is_project, is_valid_date};
+
 pub struct Todo {
     is_completed: bool,
     completion_date: Option<String>,
@@ -71,17 +73,71 @@ impl Todo {
     }
 
     pub fn from_formatted_string(formatted_string: &str) -> Todo {
-        let vec: Vec<&str> = formatted_string.split_whitespace().collect();
-        let priority = vec[0];
-        let content = vec[1];
+        let mut is_completed: bool = false;
+        let mut priority: Option<String> = None;
+        let mut completion_date: Option<String> = None;
+        let mut creation_date: Option<String> = None;
+        let mut content: String;
+        let mut projects: Option<Vec<String>> = None;
+        let mut contexts: Option<Vec<String>> = None;
+        let mut iter = formatted_string.split_whitespace();
+        let mut value: &str = iter.next().unwrap();
+
+        if value.eq("x") {
+            is_completed = true;
+            value = iter.next().unwrap();
+        }
+
+        if value.starts_with("(") {
+            priority = Some(value.chars().nth(1).unwrap().to_string());
+            value = iter.next().unwrap();
+        }
+        if is_valid_date(value) {
+            let date1: Option<String> = Some(value.to_string());
+            value = iter.next().unwrap();
+            if is_valid_date(value) {
+                let date2: Option<String> = Some(value.to_string());
+                value = iter.next().unwrap();
+                completion_date = date1;
+                creation_date = date2;
+            } else {
+                if is_completed {
+                    completion_date = date1;
+                } else {
+                    creation_date = date1;
+                }
+            }
+        }
+
+        content = value.to_string();
+
+        let mut projects_vec: Vec<String> = [].to_vec();
+        let mut contexts_vec: Vec<String> = [].to_vec();
+        for val in iter {
+            if is_project(val) {
+                projects_vec.push(val[1..].to_string());
+            } else if is_context(val) {
+                contexts_vec.push(val[1..].to_string());
+            } else {
+                content += &(" ".to_string() + val);
+            }
+        }
+
+        if !projects_vec.is_empty() {
+            projects = Some(projects_vec);
+        }
+        if !contexts_vec.is_empty() {
+            contexts = Some(contexts_vec);
+        }
+
         Todo {
-            is_completed: false,
-            completion_date: None,
-            content: content.to_string(),
-            priority: Some(priority.to_string()),
-            creation_date: None,
-            projects: None,
-            contexts: None,
+            is_completed,
+            completion_date,
+            content,
+            priority,
+            creation_date,
+            projects,
+            contexts,
         }
     }
 }
@@ -231,18 +287,110 @@ mod tests {
 
     mod from_formatted_string {
         #[test]
-        fn from_content() {
-            let formatted_string = "todo test";
+        fn from_flag() {
+            let formatted_string = "x todo text";
             let todo = super::Todo::from_formatted_string(formatted_string);
-            assert_eq!(todo.content, "todo test");
+            assert!(todo.is_completed);
         }
 
         #[test]
-        fn from_priority_content() {
-            let formatted_string = "(A) todo test";
+        fn from_priority() {
+            let formatted_string = "(A) todo text";
             let todo = super::Todo::from_formatted_string(formatted_string);
+            assert!(!todo.is_completed);
             assert_eq!(todo.priority, Some("A".to_string()));
-            assert_eq!(todo.content, "todo test");
+            assert_eq!(todo.content, "todo text".to_string());
+        }
+
+        #[test]
+        fn from_flag_priority() {
+            let formatted_string = "x (A) todo text";
+            let todo = super::Todo::from_formatted_string(formatted_string);
+            assert!(todo.is_completed);
+            assert_eq!(todo.priority, Some("A".to_string()));
+            assert_eq!(todo.content, "todo text".to_string());
+        }
+
+        #[test]
+        fn from_priority_creation_date() {
+            let formatted_string = "(A) 2000-1-1 todo text";
+            let todo = super::Todo::from_formatted_string(formatted_string);
+            assert!(!todo.is_completed);
+            assert_eq!(todo.priority, Some("A".to_string()));
+            assert_eq!(todo.completion_date, None);
+            assert_eq!(todo.creation_date, Some("2000-1-1".to_string()));
+            assert_eq!(todo.content, "todo text".to_string());
+        }
+
+        #[test]
+        fn from_flag_priority_completion_date() {
+            let formatted_string = "x (A) 2000-1-1 todo text";
+            let todo = super::Todo::from_formatted_string(formatted_string);
+            assert!(todo.is_completed);
+            assert_eq!(todo.priority, Some("A".to_string()));
+            assert_eq!(todo.completion_date, Some("2000-1-1".to_string()));
+            assert_eq!(todo.creation_date, None);
+            assert_eq!(todo.content, "todo text".to_string());
+        }
+
+        #[test]
+        fn from_flag_priority_completion_date_creation_date() {
+            let formatted_string = "x (A) 2000-1-1 1999-12-31 todo text";
+            let todo = super::Todo::from_formatted_string(formatted_string);
+            assert!(todo.is_completed);
+            assert_eq!(todo.priority, Some("A".to_string()));
+            assert_eq!(todo.completion_date, Some("2000-1-1".to_string()));
+            assert_eq!(todo.creation_date, Some("1999-12-31".to_string()));
+            assert_eq!(todo.content, "todo text".to_string());
+        }
+
+        #[test]
+        fn from_content_projects() {
+            let formatted_string = "todo text +projectA +projectB";
+            let todo = super::Todo::from_formatted_string(formatted_string);
+            assert!(!todo.is_completed);
+            assert_eq!(todo.priority, None);
+            assert_eq!(todo.completion_date, None);
+            assert_eq!(todo.creation_date, None);
+            assert_eq!(todo.content, "todo text".to_string());
+            assert_eq!(
+                todo.projects,
+                Some(vec!["projectA".to_string(), "projectB".to_string()])
+            );
+        }
+
+        #[test]
+        fn from_content_contexts() {
+            let formatted_string = "todo text @contextA @contextB";
+            let todo = super::Todo::from_formatted_string(formatted_string);
+            assert!(!todo.is_completed);
+            assert_eq!(todo.priority, None);
+            assert_eq!(todo.completion_date, None);
+            assert_eq!(todo.creation_date, None);
+            assert_eq!(todo.content, "todo text".to_string());
+            assert_eq!(
+                todo.contexts,
+                Some(vec!["contextA".to_string(), "contextB".to_string()])
+            );
+        }
+
+        #[test]
+        fn from_content_projects_contexts() {
+            let formatted_string = "todo text +projectA +projectB @contextA @contextB";
+            let todo = super::Todo::from_formatted_string(formatted_string);
+            assert!(!todo.is_completed);
+            assert_eq!(todo.priority, None);
+            assert_eq!(todo.completion_date, None);
+            assert_eq!(todo.creation_date, None);
+            assert_eq!(todo.content, "todo text".to_string());
+            assert_eq!(
+                todo.projects,
+                Some(vec!["projectA".to_string(), "projectB".to_string()])
+            );
+            assert_eq!(
+                todo.contexts,
+                Some(vec!["contextA".to_string(), "contextB".to_string()])
+            );
         }
     }
 }
