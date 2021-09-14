cc6b80d843cde8c8502f44bd9ef5f2c692dee3b1
Kazuaki Nomura <nkz0914ssk@gmail.com>
2021-09-14 22:16:39 +0900

    :sparkles:  add json loader

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
 use clap::{load_yaml, App};
+use serde::{Deserialize, Serialize};
+use std::{fs::OpenOptions, io::BufReader};
 use todo_txt::constants::subcommands::*;
 use todo_txt::service::TodoService;
 
+#[derive(Serialize, Deserialize, Debug)]
+struct Config {
+    default_file_name: String,
+}
+
 fn main() {
+    let config: Config = read_config();
+    println!("{:?}", config);
     let yaml = load_yaml!("cli.yaml");
     let m = App::from(yaml).get_matches();
     let service = TodoService::new("todo.txt");
@@ -20,4 +29,14 @@ fn main() {
     if let Some(_) = m.subcommand_matches(LIST) {
         service.list_todos();
     }
+}
+
+fn read_config() -> Config {
+    let config_file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .open("config.json")
+        .unwrap();
+    let reader = BufReader::new(config_file);
+    serde_json::from_reader(reader).unwrap()
 }
