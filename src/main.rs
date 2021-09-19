7b06f82e1e4089f91ab61c6ec386c9cc2f59e25e
Kazuaki Nomura <nkz0914ssk@gmail.com>
2021-09-19 22:45:58 +0900

    :sparkles: (options) add filename option

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use clap::{load_yaml, App};
 use todo_txt::config::Config;
+use todo_txt::constants::options::*;
 use todo_txt::constants::subcommands::*;
 use todo_txt::service::TodoService;
 
@@ -7,8 +8,7 @@ fn main() {
     let config = Config::new();
     let yaml = load_yaml!("cli.yaml");
     let m = App::from(yaml).get_matches();
-    let service = TodoService::new(&config.default_file_name);
-
+    let service = TodoService::new(m.value_of(FILE).unwrap_or(&config.default_file_name));
     if let Some(m) = m.subcommand_matches(ADD) {
         let todo_string = service.add_todo(m);
         println!("Created: {:?}", todo_string);
